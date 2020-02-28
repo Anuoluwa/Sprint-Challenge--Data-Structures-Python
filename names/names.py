a2413e7fceb89951bfc6856125ba13c660d65c9a
Anuoluwa Apiti <anuoluwa.apiti@gmail.com>
2020-02-28 10:16:46 +0100

    complete solutions for names.py

--- names/names.py
+++ names/names.py
@@ -1,5 +1,7 @@
 import time
 
+from collections import Counter, deque
+
 start_time = time.time()
 
 f = open('names_1.txt', 'r')
@@ -10,11 +12,24 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+names_1 = Counter(names_1)
+names_2 = Counter(names_2)
+
+# this implementation uses other external resources online
+
+# duplicates = [] # this will runtime: 0.02987384796142578 seconds
+
+duplicates = deque() #runtime: 0.012484073638916016 seconds
+
+
+for name in names_1:
+    if name in names_2:
+        duplicates.append(name)
+
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
