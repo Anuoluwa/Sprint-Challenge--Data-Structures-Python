bfcb729d4e0c7b0aa307c42272914ee8de96b83d
Anuoluwa Apiti <anuoluwa.apiti@gmail.com>
2020-02-28 09:42:59 +0100

    complete solutions for ring_buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -8,15 +8,25 @@ class RingBuffer:
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+        if self.current == self.capacity:
+            # set the self.current to 0 again
+            self.current = 0
+
+            # insert into the storage at position current
+        self.storage[self.current] = item
+        # increment current
+        self.current += 1
 
     def get(self):
         # Note:  This is the only [] allowed
-        list_buffer_contents = []
+        #list_buffer_contents = []
 
         # TODO: Your code here
+        # return the element in the storage
+        # do not return None, use list comprehension
+        # return list_buffer_contents
 
-        return list_buffer_contents
+        return [item for item in self.storage if item is not None]
 
 # ----------------Stretch Goal-------------------
 
